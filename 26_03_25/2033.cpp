3e781825cd247771c0896fcfc7ccea74e92db8c9
VikramChauhan19 <vikram9696chauhan@gmail.com>
2025-03-26 21:57:06 +0530

    medium

---
+++ 26_03_25/2033.cpp
@@ -0,0 +1,24 @@
+class Solution {
+    public:
+        int minOperations(vector<vector<int>>& grid, int x) {
+            vector<int>vals;
+            for(auto i:grid){
+                for(auto j:i){
+                    vals.push_back(j);
+                }
+            }
+            sort(vals.begin(),vals.end());
+            int n = vals.size();
+            int mid = n/2;
+            int count = 0;
+            for(int i = 0;i<n;i++){
+                int diff = abs(vals[i]-vals[mid]);
+                if(diff%x == 0){
+                    count += diff/x;
+                }
+                else return -1;
+            }
+            return count;
+            
+        }
+    };
