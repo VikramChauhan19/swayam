8939d1a61aba4db5afb15b4b55d84b8ccf2a9e2d
VikramChauhan19 <vikram9696chauhan@gmail.com>
2025-03-26 22:10:40 +0530

    POTD 26-03-25

--- 26_03_25/2033. Minimum Operations to Make a Uni-Value Grid.cpp
+++ 26_03_25/2033. Minimum Operations to Make a Uni-Value Grid.cpp
@@ -6,19 +6,15 @@ class Solution {
         int minOperations(vector<vector<int>>& grid, int x) {
             vector<int>vals;
             for(auto i:grid){
-                for(auto j:i){
-                    vals.push_back(j);
-                }
+                for(auto j : i) vals.push_back(j);
             }
             sort(vals.begin(),vals.end());
             int n = vals.size();
             int mid = n/2;
             int count = 0;
             for(int i = 0;i<n;i++){
-                int diff = abs(vals[i]-vals[mid]);
-                if(diff%x == 0){
-                    count += diff/x;
-                }
+                int diff = abs(vals[i] - vals[mid]);
+                if(diff % x == 0) count += diff/x;
                 else return -1;
             }
             return count;
