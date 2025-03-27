71a2c946ee8b235468683172372a2b31ccf3c370
VikramChauhan19 <vikram9696chauhan@gmail.com>
2025-03-27 11:17:03 +0530

    POTD 27-03-25

--- 27_03_25/2780. Minimum Index of a Valid Split.cpp
+++ 27_03_25/2780. Minimum Index of a Valid Split.cpp
@@ -1,6 +1,7 @@
 #include<vector>
 #include<unordered_map>
 using namespace std;
+//Soln -> 1
 class Solution {
     public:
         int minimumIndex(vector<int>& nums) {    //TC -> n+n -> O(N)
@@ -22,5 +23,43 @@ class Solution {
                 }
             }
             return -1;        
+        }
+    };
+
+//SOln -> 2 (optimized)
+class Solution {
+    public:
+        int majorityE(vector<int>&nums){ // TC = O(n)
+            int count = 0;               // SC = O(1)
+            int cand = 0;
+            for(auto i:nums){
+                if(count == 0) cand = i;
+                if(cand == i) count++;
+                else count--;
+            }
+            return cand;
+    
+        }
+        int minimumIndex(vector<int>& nums) {
+            int n = nums.size();
+            
+            int c = 0;
+            int cand = majorityE(nums);
+            int remain = count(nums.begin(),nums.end(),cand);
+            for(int i = 0;i<n;i++){
+                int num = nums[i];
+                if(num == cand){
+                    c++;
+                    remain--;
+                }
+                int n1 = i+1;
+                int n2 = n-i-1;
+                if(c>n1/2 && remain > n2/2){
+                    return i;
+                }
+            }
+            return -1;
+    
+            
         }
     };
