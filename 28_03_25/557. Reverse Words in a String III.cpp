a0fb65456ec6e78051fda13ea953621ea331c34a
VikramChauhan19 <vikram9696chauhan@gmail.com>
2025-03-28 13:52:22 +0530

    Easy(Two pointer, string)

---
+++ 28_03_25/557. Reverse Words in a String III.cpp
@@ -0,0 +1,23 @@
+#include<string>
+#include<sstream>
+using namespace std;
+class Solution {
+    public:
+       
+        string reverseWords(string s) {
+           
+            istringstream ss(s);
+            string word;
+            string ans;
+            while(ss>>word){
+                reverse(word.begin(),word.end());
+                ans+= (word +" ");
+    
+            }
+            return ans.substr(0,ans.size()-1);
+    
+            
+        }
+            
+        
+    };
