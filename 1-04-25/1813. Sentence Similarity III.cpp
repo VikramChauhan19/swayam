e0c406ad5add117d1b40423dc4c8c2e591567d56
VikramChauhan19 <vikram9696chauhan@gmail.com>
2025-04-01 14:00:09 +0530

    Medium(deque)

---
+++ 1-04-25/1813. Sentence Similarity III.cpp
@@ -0,0 +1,37 @@
+#include<string>
+#include<sstream>
+#include<deque>
+
+using namespace std;
+class Solution {
+    public:
+        bool areSentencesSimilar(string sentence1, string sentence2) {// TC = O(n) sp = O(1)
+            if(sentence2.size()> sentence1.size()){
+                swap(sentence1,sentence2); //o(1)
+            }
+            deque<string> deq1,deq2;
+    
+            stringstream ss1(sentence1);
+            string token;
+            while(ss1>>token){  //O(n1)
+                deq1.push_back(token);
+            }
+            
+            stringstream ss2(sentence2);
+            
+            while(ss2>>token){   //O(n2)
+                deq2.push_back(token);
+            }
+    
+            while(!deq1.empty() && !deq2.empty() && deq1.front() == deq2.front()){ //O(n)
+                deq1.pop_front();
+                deq2.pop_front();
+            }
+            while(!deq1.empty() && !deq2.empty() && deq1.back() == deq2.back()){
+                deq1.pop_back();
+                deq2.pop_back();
+            }
+            return deq2.empty();
+            
+        }
+    };
